d06a23fdc3760bd34b874c77de05bdf1b91e5b80
Bryan Colin <bryancolin35@ymail.com>
2021-07-09 15:18:21 +0700

    Initial Set Up
    Set up Color Palette & App Icon

--- Filmies/Views/ContentView.swift
+++ Filmies/Views/ContentView.swift
@@ -6,11 +6,13 @@
 //
 
 import SwiftUI
+import UIKit
 
 struct ContentView: View {
     var body: some View {
         Text("Hello, world!")
             .padding()
+            .foregroundColor(Color("BrandPink"))
     }
 }
 
