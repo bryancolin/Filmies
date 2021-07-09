d0baa117226da5fb45124d24c78294b314023cb1
Bryan Colin <bryancolin35@ymail.com>
2021-07-09 10:40:14 +0700

    Initial Commit

---
+++ Filmies/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Filmies
+//
+//  Created by bryan colin on 7/9/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Filmies/FilmiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FilmiesApp.swift
+//  Filmies
+//
+//  Created by bryan colin on 7/9/21.
+//
+
+import SwiftUI
+
+@main
+struct FilmiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
