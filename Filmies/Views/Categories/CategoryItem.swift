a5770b1cbfd8b8ac3c007a13b7515417ff6cd022
Bryan Colin <bryancolin35@ymail.com>
2021-07-19 14:35:47 +0700

    Update CategoryItem View

--- Filmies/Views/Categories/CategoryItem.swift
+++ Filmies/Views/Categories/CategoryItem.swift
@@ -21,30 +21,31 @@ struct CategoryItem: View {
     
     var body: some View {
         VStack(alignment: .leading) {
-            WebImage(url: URL(string: movie.imageURL))
-                .resizable()
-                .frame(width: width, height: height, alignment: .leading)
-                .cornerRadius(8)
-                .overlay(
-                    Circle()
-                        .frame(width: 25, height: 25)
-                        .foregroundColor(Color("BrandPink"))
-                        .overlay(
-                            Text(movie.releaseYear)
-                                .font(.system(size: 9))
-                                .foregroundColor(.white)
-                        )
-                        .padding(EdgeInsets(top: 5, leading: 5, bottom: 0, trailing: 0))
-                    , alignment: .topLeading)
+            Button(action: {
+                if movie.details == false {
+                    modelData.fetchMovieDetails(param: category, id: movie.id)
+                }
+                showingModal.toggle()
+            }, label: {
+                WebImage(url: URL(string: movie.imageURL))
+                    .resizable()
+                    .frame(width: width, height: height, alignment: .leading)
+                    .cornerRadius(8)
+                    .overlay(
+                        Circle()
+                            .frame(width: 25, height: 25)
+                            .foregroundColor(Color("BrandPink"))
+                            .overlay(
+                                Text(movie.releaseYear)
+                                    .font(.system(size: 9))
+                                    .foregroundColor(.white)
+                            )
+                            .padding(EdgeInsets(top: 5, leading: 5, bottom: 0, trailing: 0))
+                        , alignment: .topLeading)
+            })
         }
         .padding(.leading, 15)
         .padding(.vertical)
-        .onTapGesture {
-            if movie.details == false {
-                modelData.fetchMovieDetails(param: category, id: movie.id)
-            }
-            showingModal.toggle()
-        }
         .sheet(isPresented: $showingModal) {
             ModalView(movie: movie, category: category, showModal: self.$showingModal)
                 .environmentObject(modelData)
