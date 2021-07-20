1fb790c8ec8fea5186ee3448bace3a2444c6e933
Bryan Colin <bryancolin35@ymail.com>
2021-07-20 19:27:10 +0700

    Create Constants.swift

---
+++ Filmies/Resources/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  Filmies
+//
+//  Created by bryan colin on 7/20/21.
+//
+
+import Foundation
+
+struct K {
+    
+    struct BrandColors {
+        static let blue = "BrandBlue"
+        static let darkBlue = "BrandDarkBlue"
+        static let pink = "BrandPink"
+        static let purple = "BrandPurple"
+    }
+}
