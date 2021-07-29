06e2025aece04754d1439fc652d9e35e6239e549
Bryan Colin <bryancolin35@ymail.com>
2021-07-29 18:56:15 +0700

    Update MovieTrailer View

--- Filmies/Views/Movie/MovieTrailer.swift
+++ Filmies/Views/Movie/MovieTrailer.swift
@@ -12,22 +12,22 @@ struct MovieTrailer: View {
     var movie: Movie
     
     var body: some View {
-        if let trailers = movie.videos?.all, !trailers.isEmpty {
-            VStack {
-                let officialTrailers = trailers.filter { trailer -> Bool in
-                    guard let trailerName = trailer.name else { return false }
-                    return trailerName.contains("Trailer")
+        if let officialTrailers = movie.videos?.all?.filter({ trailer -> Bool in
+            guard let trailerName = trailer.name else { return false }
+            return trailerName.contains("Trailer")
+        }) {
+            if !officialTrailers.isEmpty {
+                VStack {
+                    if officialTrailers.count == 1 {
+                        WebPlayerView(urlString: officialTrailers.first?.youtubeURL, loadOnce: true)
+                    } else {
+                        PageView(pages: officialTrailers.compactMap({ WebPlayerView(urlString: $0.youtubeURL, loadOnce: true) }), alignment: .topTrailing)
+                    }
                 }
-                
-                if officialTrailers.count == 1 {
-                    WebPlayerView(urlString: officialTrailers.first?.youtubeURL, loadOnce: true)
-                } else {
-                    PageView(pages: officialTrailers.compactMap({ WebPlayerView(urlString: $0.youtubeURL, loadOnce: true) }), alignment: .topTrailing)
-                }
+                .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height / 3, alignment: .top)
+            } else {
+                CustomImage(urlString: movie.imageURL)
             }
-            .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height / 3, alignment: .top)
-        } else {
-            CustomImage(urlString: movie.imageURL)
         }
     }
 }
