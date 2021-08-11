0f266bd7963a5e29a7fe726fb13227459beedb65
Bryan Colin <bryancolin35@ymail.com>
2021-08-11 12:02:13 +0700

    Update Cast.swift
    Get Character

--- Filmies/Model/Cast/Cast.swift
+++ Filmies/Model/Cast/Cast.swift
@@ -9,7 +9,7 @@ import Foundation
 
 struct Cast: Codable, Identifiable {
     let id: Int?
-    let name, department: String?
+    let name, character, department: String?
     let order: Int?
     
     let profilePicture: String?
@@ -23,6 +23,7 @@ struct Cast: Codable, Identifiable {
     enum CodingKeys: String, CodingKey {
         case id
         case name = "original_name"
+        case character
         case department = "known_for_department"
         case order
         case profilePicture = "profile_path"
