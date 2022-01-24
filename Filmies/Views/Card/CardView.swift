ac2347f9e06960ecd6d1f3a9fdafeae4074fceb9
Bryan Colin <53321614+bryancolin@users.noreply.github.com>
2022-01-24 15:11:01 +0700

    Update CardView

--- Filmies/Views/Card/CardView.swift
+++ Filmies/Views/Card/CardView.swift
@@ -18,12 +18,50 @@ struct CardView: View {
     @State var scrolled = 0
     @State var offsets: [CGFloat] = Array(repeating: 0, count: 20)
     
+    var drag: some Gesture {
+        DragGesture()
+            .onChanged { value in
+                let translation = value.translation.width
+                withAnimation {
+                    if translation < 0 && scrolled < (modelData.films[category]?.count ?? 0) - 1{
+                        offsets[scrolled] = translation
+                    } else {
+                        if scrolled > 0 {
+                            offsets[scrolled - 1] = -(calculateWidth() + 60) + translation
+                        }
+                    }
+                }
+            }
+            .onEnded { value in
+                let translation = value.translation.width
+                withAnimation {
+                    if translation < 0 {
+                        if -translation > 180 && scrolled < (modelData.films[category]?.count ?? 0) - 1 {
+                            offsets[scrolled] = -(calculateWidth() + 60)
+                            scrolled += 1
+                        } else {
+                            offsets[scrolled] = 0
+                        }
+                    } else {
+                        if scrolled > 0 {
+                            if translation > 180 {
+                                offsets[scrolled - 1] = 0
+                                scrolled -= 1
+                            } else {
+                                offsets[scrolled - 1] = -(calculateWidth() + 60)
+                            }
+                        }
+                    }
+                }
+            }
+    }
+    
     //MARK: - BODY
     
     var body: some View {
         ZStack {
             if let films = modelData.films[category] {
-                if modelData.isLoading == false {
+                if !modelData.isLoading {
                     ForEach(films.enumerated().reversed(), id: \.offset) { index, film in
                         HStack {
                             ZStack(alignment: .bottomLeading) {
@@ -35,43 +73,7 @@ struct CardView: View {
                         } //: HSTACK
                         .contentShape(Rectangle())
                         .offset(x: offsets[index])
-                        .gesture(
-                            DragGesture()
-                                .onChanged { value in
-                                    let translation = value.translation.width
-                                    withAnimation {
-                                        if translation < 0 && index < (modelData.films[category]?.count ?? 0) - 1{
-                                            offsets[index] = translation
-                                        } else {
-                                            if index > 0 {
-                                                offsets[index - 1] = -(calculateWidth() + 60) + translation
-                                            }
-                                        }
-                                    }
-                                }
-                                .onEnded { value in
-                                    let translation = value.translation.width
-                                    withAnimation {
-                                        if translation < 0 {
-                                            if -translation > 180 && index < (modelData.films[category]?.count ?? 0) - 1 {
-                                                offsets[index] = -(calculateWidth() + 60)
-                                                scrolled += 1
-                                            } else {
-                                                offsets[index] = 0
-                                            }
-                                        } else {
-                                            if index > 0 {
-                                                if translation > 180 {
-                                                    offsets[index - 1] = 0
-                                                    scrolled -= 1
-                                                } else {
-                                                    offsets[index - 1] = -(calculateWidth() + 60)
-                                                }
-                                            }
-                                        }
-                                    }
-                                }
-                        ) //: GESTURE
+                        .gesture(drag) //: GESTURE
                     } //: LOOP
                 } else {
                     Color.white.opacity(0.2)
